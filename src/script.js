bd1a8423f846850a34376c403f482ef8732e19dd
YOUCEF54 <94319560+YOUCEF54@users.noreply.github.com>
2024-07-06 19:10:30 +0100

    updates on index.html

--- src/script.js
+++ src/script.js
@@ -1,15 +1,12 @@
-// Get the modal
 var modal = document.getElementById("modal");
 
-// Get the button that opens the modal
 var btn = document.getElementById("btn");
 
-// Get the <span> element that closes the modal
 var span = document.getElementById("close");
+
 
 var backModal = document.getElementById("backModal");
 
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
      backModal.style.display = "flex";
     setTimeout(()=> modal.style.height = "12rem"  , 100);
@@ -23,14 +20,14 @@ backModal.onclick = function() {
 
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.height = "0rem";
     setTimeout(()=>  backModal.style.display = "none"  , 400);
 
 }
 
-// When the user clicks anywhere outside of the modal, close it
+/////////////////////////////////////////////
+
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.width = "8%";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'san serif'],
+        lexend: ['Lexend Deca', 'sans-serif']
+
+      }
+    },
   },
   plugins: [],
 }
